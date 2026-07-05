34be343ededbd02147f1f44b10d7172894a06f8c
MaithreyanJK <maithreyanjk@gmail.com>
2026-07-05 13:16:40 +0530

    np.array, np.ones, np.zeros

---
+++ learning_numpy/first_numpy_code.py
@@ -0,0 +1,5 @@
+import numpy as np
+num_1 = np.array([5,10,15,20,20])
+print(num_1)
+num_2 = np.array([100,200,300,400])
+print(num_2)
---
+++ learning_numpy/practice1.py
@@ -0,0 +1,14 @@
+import numpy as np
+zeros = np.zeros(8)
+print(zeros)
+
+ones = np.ones(6)
+print(ones)
+
+integer_zeros = np.zeros(10, dtype=int)
+print(integer_zeros)
+
+integer_ones = np.ones(7, dtype=int)
+print(integer_ones)
+
+
