5dc1ecfa753682c638227a1bfdaf28a4275923ca
MaithreyanJK <maithreyanjk@gmail.com>
2026-08-06 19:14:52 +0530

    Add solution for Level 2 - Q003 Employee Salary Analysis

---
+++ Numpy_practice/level_2/q003_employee_salary_analysis.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+employees_salaries = []
+for i in range(1, 9):
+    salaries = int(input(f"enter employee salary {i}: "))
+    employees_salaries.append(salaries)
+
+salaries_array = np.array(employees_salaries)
+
+high_salary = salaries_array[salaries_array >= 30000]
+low_salary = salaries_array[salaries_array < 30000]
+inclusive = salaries_array[(salaries_array >= 30000) & (salaries_array =< 50000)]
+
+print(f"Original salaries: {salaries_array}")
+print(f"high salaries: {high_salary}")
+print(f"low salaries: {low_salary}")
+print(f"Between 30000 to 50000: {inclusive}")
+print(f"high salary count: {high_salary.size}")
+print(f"low salary count: {low_salary.size}")
+print(f"Between 30k to 50k count: {inclusive.size}")
+
+
+
+
