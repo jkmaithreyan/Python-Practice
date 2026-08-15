6f6e1e01faed6257a0f6e4aa08cc8e49b1343592
MaithreyanJK <maithreyanjk@gmail.com>
2026-08-15 13:24:26 +0530

    Add solution for Level 2 - Q006 Exam Marks Analysis

---
+++ Numpy_practice/level_2/q006_exam_marks_analysis.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+marks = []
+
+for i in range(1, 9):
+    mark = int(input(f"Enter mark {i}: "))
+    marks.append(mark)
+
+marks_array = np.array(marks)
+
+excellent_marks = marks_array[marks_array >= 80]
+average_marks = marks_array[(marks_array <= 79) & (marks_array >= 50)]
+low_marks = marks_array[marks_array < 50]
+percentage = excellent_marks.size / marks_array.size * 100
+
+print(f"Original Marks: {marks_array}")
+print(f"Excellent Marks: {excellent_marks}")
+print(f"Average Marks: {average_marks}")
+print(f"Low Marks: {low_marks}")
+
+print(f"""
+Excellent marks Count: {excellent_marks.size}
+Average marks Count: {average_marks.size}
+Low marks Count: {low_marks.size}""")
+
+print(f"Excellent Percentage: {percentage}")
